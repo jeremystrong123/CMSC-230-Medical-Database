a8c77818e6ac5c1c38c37ee44e0634a804096ff4
Jeremy <w1325363@myfcc.frederick.edu>
2026-03-02 04:13:32 +0000

    assignment 2 patient list finish

--- Medical Database/PatientList.java
+++ Medical Database/PatientList.java
@@ -1,3 +1,123 @@
 public class PatientList {
+    //instance variables
+    private Patient[] patientArray;
+    private int nextAvailableIndex;
+    private int indexOfIteration;
 
+    //constructors
+    /**
+     * Constructs a PatientList object that has a patientArray with 1000 maximum patients
+     * Sets the nextAvailableIndex to 0 as the list should be empty
+     * Sets the indexOfIteration to -1 as the empty list cannot be iterated through
+     */
+    public PatientList() {
+        patientArray = new Patient[1000];
+        nextAvailableIndex = 0;
+        indexOfIteration = -1;
+    }
+
+    //methods
+    /**
+     * Add method to add a Patient to the PatientList in the correct order based off of their PatientIdentity.
+     * 
+     * @param p Patient to be added to the PatientList.
+     * 
+     * @return true if sucessful false if not
+     */
+    public boolean addPatient(Patient p) {
+        int currentIndex = nextAvailableIndex - 1;
+        if (nextAvailableIndex == 0) {
+            patientArray[0] = p;
+            nextAvailableIndex++;
+            return true;
+        }
+        else if (nextAvailableIndex < patientArray.length) {
+            while ((currentIndex >= 0) && p.getIdentity().isLessThan(patientArray[currentIndex].getIdentity())) {
+                patientArray[currentIndex + 1] = patientArray[currentIndex];
+                patientArray[currentIndex] = null;
+                currentIndex--;
+            }
+            patientArray[currentIndex + 1] = p;
+            nextAvailableIndex++;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Locates a patient inside of the PatientList using their identity through a binary search.
+     * 
+     * @param id PatientIdentity of the patient you want to find
+     * 
+     * @return Returns the Patient object with the given patient identity if found and returns null if not found.
+     */
+    public Patient findPatient(PatientIdentity id) {
+        int min = 0;
+        int max = nextAvailableIndex;
+        while (min < max) {
+            int mid = (max + min) / 2;
+            if (patientArray[mid].getIdentity().match(id)) {
+                return patientArray[mid];
+            }
+            else if (patientArray[mid].getIdentity().isLessThan(id)){
+                min = mid + 1;
+            }
+            else {
+                max = mid;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Initializes iteration from the start of the array, setting the indexOfIteration to 0
+     * if the patientList's array contains any Patient objects, and keeping it at -1 if the
+     * array is empty.
+     */
+    public void initIteration() {
+        if (patientArray[0] != null) {
+            indexOfIteration = 0;
+        }
+        else {
+            indexOfIteration = -1;
+        }
+    }
+
+    /**
+     * Finds the current patient in the array and then iterates the index onto the next patient in the array
+     * resets the index if there are no more patients in the array
+     * 
+     * @return returns the patient at the current index of iteration, or null if there are no patients in the array
+     * or if the index has not been initialized
+     */
+    public Patient next() {
+        Patient current;
+        if (indexOfIteration == -1) {
+            return null;
+        }
+        else {
+            current = patientArray[indexOfIteration];
+        }
+        indexOfIteration++;
+        if (patientArray[indexOfIteration] == null) {
+            indexOfIteration = -1;
+        }
+        return current;
+    }
+
+    /**
+     * Get method for the patient list's array
+     * @return returns the patientArray
+     */
+    public Patient[] getPatientArray() {
+        return patientArray;
+    }
+
+    /**
+     * Get method for the indexOfIteration
+     * @return returns the indexOfIteration's integer value
+     */
+    public int getIndexOfIteration() {
+        return indexOfIteration;
+    }
 }
--- Tests/PatientListTest.java
+++ Tests/PatientListTest.java
@@ -1,3 +1,124 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
 public class PatientListTest {
 
+    private Name name;
+    private Date dob;
+    private PatientIdentity id;
+    private Patient pt;
+    private PatientList pl;
+
+    @BeforeEach
+    void createListAndPatient() {
+        name = new Name("John", "Smith");
+        dob = new Date(90, 2, 2);
+        id = new PatientIdentity(name, dob);
+        pt = new Patient(id);
+        pl = new PatientList();
+    }
+
+    @Test
+    void addPatientTestForTrueAndWorks() {
+        assertEquals(true, pl.addPatient(pt));
+        assertEquals(pt, pl.getPatientArray()[0]);
+    }
+
+    @Test
+    void addPatientFalseWhenFull() {
+        for (int i = 0; i<1000; i++) {
+            pl.addPatient(pt);
+        }
+        assertEquals(false, pl.addPatient(pt));
+    }
+
+    @Test
+    void addPutsLesserPatientBefore() {
+        pl.addPatient(pt);
+        Name name2 = new Name("A", "A");
+        PatientIdentity id2 = new PatientIdentity(name2, dob);
+        Patient pt2 = new Patient(id2);
+        pl.addPatient(pt2);
+        assertEquals(pt2, pl.getPatientArray()[0]);
+    }
+
+    @Test
+    void addPutsGreaterPatientAfter() {
+        pl.addPatient(pt);
+        Name name2 = new Name("Z", "Z");
+        PatientIdentity id2 = new PatientIdentity(name2, dob);
+        Patient pt2 = new Patient(id2);
+        pl.addPatient(pt2);
+        assertEquals(pt2, pl.getPatientArray()[1]);
+    }
+
+    @Test
+    void findPatientWorks() {
+        pl.addPatient(pt);
+        Name name2 = new Name("Y", "Y");
+        PatientIdentity id2 = new PatientIdentity(name2, dob);
+        Patient pt2 = new Patient(id2);
+        pl.addPatient(pt2);
+        Name name3 = new Name("Z", "Z");
+        PatientIdentity id3 = new PatientIdentity(name3, dob);
+        Patient pt3 = new Patient(id3);
+        pl.addPatient(pt3);
+        assertEquals(pt, pl.findPatient(id));
+        assertEquals(pt2, pl.findPatient(id2));
+        assertEquals(pt3, pl.findPatient(id3));
+    }
+
+    @Test
+    void findPatientReturnsNullForPatientThatDoesNotExist() {
+        pl.addPatient(pt);
+        Name name2 = new Name("Y", "Y");
+        PatientIdentity id2 = new PatientIdentity(name2, dob);
+        Patient pt2 = new Patient(id2);
+        pl.addPatient(pt2);
+        Name name3 = new Name("Z", "Z");
+        PatientIdentity id3 = new PatientIdentity(name3, dob);
+        Patient pt3 = new Patient(id3);
+        assertEquals(null, pl.findPatient(id3));
+    }
+
+    @Test
+    void initIterationDoesNotInitializeEmptyArray() {
+        pl.initIteration();
+        assertEquals(-1, pl.getIndexOfIteration());
+    }
+
+    @Test
+    void initIterationWorks() {
+        pl.addPatient(pt);
+        pl.initIteration();
+        assertEquals(0, pl.getIndexOfIteration());
+    }
+
+    @Test
+    void iteratorReturnsNullIfNotInitialized() {
+        assertEquals(null, pl.next());
+    }
+
+    @Test
+    void iteratorReturnsProperPatientAndIteratesThroughArray() {
+        pl.addPatient(pt);
+        Name name2 = new Name("A", "A");
+        PatientIdentity id2 = new PatientIdentity(name2, dob);
+        Patient pt2 = new Patient(id2);
+        pl.addPatient(pt2);
+        Name name3 = new Name("Z", "Z");
+        PatientIdentity id3 = new PatientIdentity(name3, dob);
+        Patient pt3 = new Patient(id3);
+        pl.addPatient(pt3);
+        pl.initIteration();
+        assertEquals(pt2, pl.next());
+        assertEquals(pt, pl.next());
+        assertEquals(pt3, pl.next());
+        assertEquals(-1, pl.getIndexOfIteration());
+    }
 }
